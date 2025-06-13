5d747f58d4a93e1416f13ad159da474937cc3635
Obaidullah <Obaidullahset@gmail.com>
2025-06-13 19:30:47 +0500

    removed import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <!-- src/routes/+layout.svelte -->
 <script>
-  import '../app.css'; // if you have global styles
   import NewNav from '$lib/+Header.svelte';
-  import '../app.css';
 </script>
 
 <div class="antialiased  " >
