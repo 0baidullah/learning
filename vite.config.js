cba08a6b970a9161b8454206c44cee9bcd3c44d1
Obaidullah <Obaidullahset@gmail.com>
2025-06-16 22:51:05 +0500

    leanring

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+
 export default defineConfig({
-	assetsInclude: ['**/*.png', '**/*.jpg'],
-  plugins: [
-    tailwindcss(),
-    sveltekit(),
-  ],
-  server: {
-    host: true,
-    port: 5173,
-  },
-  
+  plugins: [sveltekit()],
 });
